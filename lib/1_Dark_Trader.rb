a0c69b393c657923bc8e428d23a96147b0623780
Rzbck <chauvel.charlie@gmail.com>
2020-04-16 19:11:04 +0200

    Updated First exercise

--- lib/1_Dark_Trader.rb
+++ lib/1_Dark_Trader.rb
@@ -2,21 +2,28 @@ require 'rubygems'
 require 'nokogiri'
 require 'rest-client'
 
+def target(page,name,xpath)
+    name = page.xpath(xpath).collect(&:text)
+    name
+end
+def zip_and_store(a,b)
+    zip_tags = a.zip(b)
+    l = []
+    zip_tags.each { |v,k|
+        h = {}
+        h.store(v,k)
+        l << h
+    }
+p l   
+end
 
 url = "https://coinmarketcap.com/all/views/all/"
 page = Nokogiri::HTML(RestClient.get(url))   
-#puts page.class   # => Nokogiri::HTML::Document
 
-symbol_tags = page.xpath("//tbody//tr//td[@class='cmc-table__cell cmc-table__cell--sortable cmc-table__cell--left cmc-table__cell--sort-by__symbol']//div/text()").collect(&:text)
-price_tags = page.xpath("//tbody//tr//td[@class='cmc-table__cell cmc-table__cell--sortable cmc-table__cell--right cmc-table__cell--sort-by__price']//a[@class='cmc-link']//text()").collect(&:text)
+symbol_xpath = "//tbody//tr//td[@class='cmc-table__cell cmc-table__cell--sortable cmc-table__cell--left cmc-table__cell--sort-by__symbol']//div/text()"
+price_xpath = "//tbody//tr//td[@class='cmc-table__cell cmc-table__cell--sortable cmc-table__cell--right cmc-table__cell--sort-by__price']//a[@class='cmc-link']//text()"
+symbol_tags , price_tags = [] , []
 
-zip_tags = symbol_tags.zip(price_tags)
-
-l = []
-zip_tags.each { |v,k|
-    h = {}
-    h.store(v,k)
-    l << h
-}
-
-p l
+symbol_tags = target(page,symbol_tags,symbol_xpath)
+price_tags = target(page,price_tags,price_xpath)
+zip_and_store(symbol_tags,price_tags)
--- lib/2_Mairie_Christmas.rb
+++ lib/2_Mairie_Christmas.rb
@@ -1,2 +1,13 @@
 require 'rubygems'
-require 'nokogiri' 
+require 'nokogiri'
+require 'rest-client'
+
+
+url = "http://annuaire-des-mairies.com/"
+page = Nokogiri::HTML(RestClient.get(url))   
+#puts page.class   # => Nokogiri::HTML::Document
+
+def get_townhall_email(townhall_url)
+end
+
+def get_townhall_urls()
